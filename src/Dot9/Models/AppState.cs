310f9977dbb5350b7877d0626f622bb2b2f22761
JGM-89 <113463105+JGM-89@users.noreply.github.com>
2026-04-30 20:41:01 +0100

    Track hotkey status changes

--- src/Dot9/Models/AppState.cs
+++ src/Dot9/Models/AppState.cs
@@ -9,21 +9,30 @@ public sealed class AppState : INotifyPropertyChanged
 {
     private Dot9Settings _settings = Dot9Settings.CreateDefault();
     private bool _overlayEnabled;
+    private string _hotkeyStatusText = "Hotkeys ready";
+    private bool _hasHotkeyWarning;
 
     public event PropertyChangedEventHandler? PropertyChanged;
     public event EventHandler? SettingsChanged;
     public event EventHandler? OverlayEnabledChanged;
+    public event EventHandler? HotkeysChanged;
 
     public Dot9Settings Settings
     {
         get => _settings;
         set
         {
+            var oldToggle = _settings.Hotkeys.ToggleOverlay;
+            var oldEmergency = _settings.Hotkeys.EmergencyOff;
             _settings = value;
             OnPropertyChanged();
             OnPropertyChanged(nameof(ActivePresetName));
             OnPropertyChanged(nameof(ActiveModeName));
             SettingsChanged?.Invoke(this, EventArgs.Empty);
+            if (oldToggle != _settings.Hotkeys.ToggleOverlay || oldEmergency != _settings.Hotkeys.EmergencyOff)
+            {
+                HotkeysChanged?.Invoke(this, EventArgs.Empty);
+            }
         }
     }
 
@@ -47,6 +56,8 @@ public sealed class AppState : INotifyPropertyChanged
     public string OverlayStatusText => OverlayEnabled ? "On" : "Off";
     public string ActivePresetName => Settings.ActivePreset;
     public string ActiveModeName => Settings.MotionMode.GetDisplayName();
+    public string HotkeyStatusText => _hotkeyStatusText;
+    public bool HasHotkeyWarning => _hasHotkeyWarning;
 
     public void ToggleOverlay() => SetOverlayEnabled(!OverlayEnabled);
 
@@ -56,16 +67,30 @@ public sealed class AppState : INotifyPropertyChanged
 
     public void Update(Action<Dot9Settings> update)
     {
+        var oldToggle = Settings.Hotkeys.ToggleOverlay;
+        var oldEmergency = Settings.Hotkeys.EmergencyOff;
         update(Settings);
         OnPropertyChanged(nameof(Settings));
         OnPropertyChanged(nameof(ActivePresetName));
         OnPropertyChanged(nameof(ActiveModeName));
         SettingsChanged?.Invoke(this, EventArgs.Empty);
+        if (oldToggle != Settings.Hotkeys.ToggleOverlay || oldEmergency != Settings.Hotkeys.EmergencyOff)
+        {
+            HotkeysChanged?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     public void ApplyPreset(PresetDefinition preset)
     {
         Settings = preset.CreateSettings();
+    }
+
+    public void SetHotkeyStatus(string message, bool isWarning = false)
+    {
+        _hotkeyStatusText = message;
+        _hasHotkeyWarning = isWarning;
+        OnPropertyChanged(nameof(HotkeyStatusText));
+        OnPropertyChanged(nameof(HasHotkeyWarning));
     }
 
     public void ApplyReducedMotionPreference()
